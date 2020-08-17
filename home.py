fae618af0d62d6265f67bd0605a9d325e943c4db
Archulan <archulan.16@cse.mrt.ac.lk>
2020-08-17 14:13:04 +0530

    home init

---
+++ home.py
@@ -0,0 +1,20 @@
+import argparse
+
+# Create the parser
+input_parser = argparse.ArgumentParser(prog='Unified intrinsic evaluation tool for word embeddings',
+                                       usage='%(prog)s [options] path',
+                                       description='Input your model and dimension size')
+
+# Add the arguments
+input_parser.add_argument('name',
+                          type=str,
+                          help='name of the model')
+
+input_parser.add_argument('dim',
+                          type=int,
+                          help='dimension of the model')
+
+# Execute the parse_args() method
+args = input_parser.parse_args()
+
+print(args,'args')
